4cf20772dd0ac6028397209b3ee140af163f26cb
Umbrae Soulsbane <umbraesoulsbane@gmail.com>
2015-02-07 19:06:40 -0500

    drawable sizes for background

--- app/src/main/java/com/sotacommunityapp/sotacommunityapp/RadioActivity.java
+++ app/src/main/java/com/sotacommunityapp/sotacommunityapp/RadioActivity.java
@@ -1,26 +1,24 @@
 package com.sotacommunityapp.sotacommunityapp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class RadioActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_radio);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_radio, menu);
         return true;
     }
 
@@ -37,9 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void radioButtonClick(View view) {
-        startActivity(new Intent(MainActivity.this, RadioActivity.class));
     }
 }
