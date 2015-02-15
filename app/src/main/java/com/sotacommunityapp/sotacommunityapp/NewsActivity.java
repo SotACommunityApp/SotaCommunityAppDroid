e3d927cca5e15587b581d9d74fadf847b290896a
Umbrae Soulsbane <umbraesoulsbane@gmail.com>
2015-02-15 01:42:41 -0500

    rss now caches
    saves xml to local storage
    refreshed xml every hour (still needs testing)

--- app/src/main/java/com/sotacommunityapp/sotacommunityapp/NewsActivity.java
+++ app/src/main/java/com/sotacommunityapp/sotacommunityapp/NewsActivity.java
@@ -1,6 +1,11 @@
 package com.sotacommunityapp.sotacommunityapp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -18,7 +23,11 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
+import android.text.format.DateFormat;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,6 +40,9 @@ public class NewsActivity extends Activity {
     Activity currentActivity = this;
     ListView listRSSItems;
     ProgressDialog waitSpinner;
+    String rssFolder = "/sotacommapp";
+    String rssFile = rssFolder + "/news.xml";
+    String rssUrl = "http://avatarsportal.com/feed";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,7 +52,6 @@ public class NewsActivity extends Activity {
         waitSpinner = new ProgressDialog(this);
         waitSpinner.setMessage("Fetching News...");
         waitSpinner.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        waitSpinner.show();
 
         listRSSItems = (ListView) findViewById(R.id.rssChannelListView);
         new GetRSSFeed().execute();
@@ -52,21 +63,41 @@ public class NewsActivity extends Activity {
         protected ArrayList<RssItem> doInBackground(Void... params) {
             ArrayList<RssItem> rssItems = null;
             try {
-
                 // TODO: Pull from local and Only fetch if hour old
-                URL url = new URL(
-                        "http://avatarsportal.com/feed");
-                RssFeed feed = RssReader.read(url);
+
+                String rssPath = Environment.getExternalStorageDirectory().getPath() + rssFile;
+                File folder = new File(Environment.getExternalStorageDirectory() + rssFolder);
+                if (!folder.exists()) {
+                    folder.mkdir();
+                }
+                File fRss = new File(rssPath);
+                RssFeed feed = null;
+
+                if(fRss.exists() && fRss.lastModified() > new Date().getTime() - 1 * DateUtils.HOUR_IN_MILLIS) {
+                    InputStream rssIs = new FileInputStream(rssPath);
+                    feed = RssReader.read(rssIs);
+                } else {
+                    waitSpinner.show();
+                    String rssResult = refreshFeed();
+                    if (rssResult == "Success") {
+                        InputStream rssIs = new FileInputStream(rssPath);
+                        feed = RssReader.read(rssIs);
+                    } else {
+                        throw new Exception();
+                    }
+                }
                 rssItems = feed.getRssItems();
 
             } catch (MalformedURLException e) {
                 Log.e("RSS Reader", "Malformed URL");
                 e.printStackTrace();
             } catch (IOException e) {
-                Log.e("RSS Reader", "Malformed URL");
+                Log.e("RSS Reader", "IOException");
                 e.printStackTrace();
             } catch (SAXException e) {
-                Log.e("RSS Reader", "Malformed URL");
+                Log.e("RSS Reader", "SAXException");
+                e.printStackTrace();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return rssItems;
@@ -104,6 +135,65 @@ public class NewsActivity extends Activity {
             waitSpinner.dismiss();
         }
 
+        public String refreshFeed()  {
+                String errOut = "Fail";
+
+                try {
+                    URL url = new URL(rssUrl);
+
+                    //create the new connection
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                    urlConnection.connect();
+
+                    //set the path where we want to save the file
+                    //in this case, going to save it on the root directory of the
+                    //sd card.
+                    File SDCardRoot = Environment.getExternalStorageDirectory();
+                    //create a new file, specifying the path, and the filename
+                    //which we want to save the file as.
+                    File file = new File(SDCardRoot, rssFile);
+
+                    //this will be used to write the downloaded data into the file we created
+                    FileOutputStream fileOutput = new FileOutputStream(file);
+
+                    //this will be used in reading the data from the internet
+                    InputStream inputStream = urlConnection.getInputStream();
+
+                    //this is the total size of the file
+                    int totalSize = urlConnection.getContentLength();
+                    waitSpinner.setMax(totalSize);
+
+                    //variable to store total downloaded bytes
+                    int downloadedSize = 0;
+
+                    //create a buffer...
+                    byte[] buffer = new byte[1024];
+                    int bufferLength = 0; //used to store a temporary size of the buffer
+
+                    //now, read through the input buffer and write the contents to the file
+                    while ((bufferLength = inputStream.read(buffer)) > 0) {
+                        //add the data in the buffer to the file in the file output stream (the file on the sd card
+                        fileOutput.write(buffer, 0, bufferLength);
+                        //add up the size so we know how much is downloaded
+                        downloadedSize += bufferLength;
+
+                    }
+                    //close the output stream when done
+                    fileOutput.close();
+                    Log.e("RSS Fetch", "Feed Saved");
+
+                    errOut = "Success";
+
+                } catch (MalformedURLException e) {
+                    Log.e("RSS Fetch", "Malformed URL");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Log.e("RSS Fetch", "IOException");
+                    e.printStackTrace();
+                }
+                return errOut;
+            }
     }
 
     public class NewsAdapter extends ArrayAdapter<NewsObj> {
@@ -145,5 +235,6 @@ public class NewsActivity extends Activity {
 
         }
     }
+
 
 }
