371cc39e8466be39ee3de42f70d9c9d9743ea219
Umbrae Soulsbane <umbraesoulsbane@gmail.com>
2015-02-15 18:22:42 -0500

    Revert "Revert "radio fixes see desc""
    This reverts commit e39eae5aee1c06a066f779e55aeba0a5b045d0f7.

--- app/src/main/java/com/sotacommunityapp/sotacommunityapp/Radio/RadioService.java
+++ app/src/main/java/com/sotacommunityapp/sotacommunityapp/Radio/RadioService.java
@@ -1,8 +1,10 @@
 package com.sotacommunityapp.sotacommunityapp.Radio;
 
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -15,6 +17,7 @@ import android.os.IBinder;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.sotacommunityapp.sotacommunityapp.R;
 import com.sotacommunityapp.sotacommunityapp.RadioActivity;
@@ -25,8 +28,13 @@ import net.moraleboost.streamscraper.Stream;
 import net.moraleboost.streamscraper.scraper.ShoutCastScraper;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
@@ -37,7 +45,7 @@ import java.util.TimerTask;
  */
 public class RadioService extends Service implements MediaPlayer.OnPreparedListener, RadioInterface {
 
-    private static final String URL = "http://cp5.digistream.info:14102";
+    private static final String scURL = "http://cp5.digistream.info:14102";
 
     /*Used to show notifications with track name*/
     private NotificationManager _notificationManager;
@@ -46,6 +54,7 @@ public class RadioService extends Service implements MediaPlayer.OnPreparedListe
     WifiManager.WifiLock _wifiLock;
     /*Media Player instance*/
     private MediaPlayer _mediaPlayer;
+    private InputStream mdIs;
 
     /*Event handlers*/
     private List<RadioListener> _listeners = new ArrayList<RadioListener>();
@@ -77,15 +86,27 @@ public class RadioService extends Service implements MediaPlayer.OnPreparedListe
     }
 
     public void Play() {
+        try {
+            _mediaPlayer.setDataSource(this, Uri.parse(scURL));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         _mediaPlayer.prepareAsync();
+
+        _wifiLock.acquire();
+
         _timer = new Timer();
-        _timer.schedule(new MetaDataTask(),0,30000);
+        _timer.schedule(new MetaDataTask(),0,10000);
     }
 
     public void Stop() {
-        if(_mediaPlayer.isPlaying())
-            _mediaPlayer.stop();
+        if(_mediaPlayer.isPlaying()) { _mediaPlayer.stop(); }
+        _mediaPlayer.reset();
+
         _timer.cancel();
+
+        _wifiLock.release();
     }
 
     @Override
@@ -116,30 +137,15 @@ public class RadioService extends Service implements MediaPlayer.OnPreparedListe
     public void onCreate() {
         _notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
-        // Display a notification about us starting.  We put an icon in the status bar.
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-                new Intent(this, RadioActivity.class), 0);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        Notification notification =  builder.setContentText("Loading...").setContentTitle("Avatar's Radio").setContentIntent(contentIntent).setSmallIcon(R.drawable.ic_stat_notify).build();
-        startForeground(NOTIFICATION, notification);
-
         if(_mediaPlayer == null) {
             _mediaPlayer = new MediaPlayer();
+            _mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
             _mediaPlayer.setOnPreparedListener(this);
             _mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+            _mediaPlayer.setVolume(.5f,.5f);
+
             _wifiLock = ((WifiManager) getSystemService(Context.WIFI_SERVICE))
                     .createWifiLock(WifiManager.WIFI_MODE_FULL, "sota_radio_lock");
-
-            _wifiLock.acquire();
-            //setVolumeControlStream(AudioManager.STREAM_MUSIC);
-            _mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            _mediaPlayer.setVolume(.5f,.5f);
-            try {
-                _mediaPlayer.setDataSource(this, Uri.parse(URL));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
         }
     }
@@ -181,19 +187,91 @@ public class RadioService extends Service implements MediaPlayer.OnPreparedListe
      * Show a notification while this service is running.
      */
     private void showNotification(String text) {
-
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, RadioActivity.class), 0);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         Notification notification =  builder.setContentText(text).setContentTitle("Avatar's Radio").setContentIntent(contentIntent).setSmallIcon(R.drawable.ic_stat_notify).build();
 
-
         // Send the notification.
         _notificationManager.notify(NOTIFICATION, notification);
     }
 
-
     private class MetaDataTask extends TimerTask {
+        private Scraper _scraper = new ShoutCastScraper();
+        private Stream _stream;
+        @Override
+        public void run() {
+            String title = null, djName = null;
+            Log.w("Radio Meta Data", "Running");
+            try {
+
+                URL updateURL = new URL(scURL);
+                URLConnection conn = updateURL.openConnection();
+                conn.setRequestProperty("Icy-MetaData", "1");
+                int interval = Integer.valueOf(conn.getHeaderField("icy-metaint")); // You can get more headers if you wish. There is other useful data.
+
+                mdIs = conn.getInputStream();
+
+                int skipped = 0;
+                while (skipped < interval) {
+                    skipped += mdIs.skip(interval - skipped);
+                }
+
+                int metadataLength = mdIs.read() * 16;
+
+                int bytesRead = 0;
+                int offset = 0;
+                byte[] bytes = new byte[metadataLength];
+
+
+                while (bytesRead < metadataLength && bytesRead != -1) {
+                    Log.w("Metadata Count", "byteRead(" + Integer.toString(bytesRead) + ") offset(" + Integer.toString(offset) + ") mdLen(" + Integer.toString(metadataLength) + ")");
+                    try {
+                        bytesRead = mdIs.read(bytes, offset, metadataLength);
+                        offset = bytesRead;
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        Log.e("Metadata Count", "byteRead(" + Integer.toString(bytesRead) + ") offset(" + Integer.toString(offset) + ") mdLen(" + Integer.toString(metadataLength) + ")");
+                        e.printStackTrace();
+                        bytesRead = 0;
+                        offset = 0;
+                    }
+                }
+
+                // StreamTitle='Ween - Gabrielle';StreamUrl='&artist=Ween&title=Gabrielle&album=&duration=208824&songtype=S&overlay=NO&buycd=&website=&picture=';
+                String[] metaData = new String(bytes).trim().split(";");
+                String outMd = "Error: " + bytes.toString().trim();
+                try {
+                    if (metaData.length > 1) {
+                        outMd = metaData[1];
+                    } else {
+                        outMd = metaData[0];
+                    }
+                } catch (NullPointerException npe) { }
+
+                //String metaData = new String(bytes).trim();
+                //title = metaData.substring(metaData.indexOf("StreamTitle='") + 13, metaData.indexOf(" / ", metaData.indexOf("StreamTitle='"))).trim();
+                //djName = metaData.substring(metaData.indexOf(" / ", metaData.indexOf("StreamTitle='")) + 3, metaData.indexOf("';", metaData.indexOf("StreamTitle='"))).trim();
+                Log.w("Radio Meta Data", outMd);
+                mdIs.close();
+
+            } catch (MalformedURLException e) { e.printStackTrace();
+            } catch (IOException e) { e.printStackTrace(); }
+
+            final String titleFin = title;
+            String djNameFin = djName;
+
+            Log.w("Radio Meta Data", "Title: " + titleFin);
+            for(RadioListener i : _listeners)
+                i.onTrackTitleChanged("Playing: " + titleFin + " (" + djNameFin + ")");
+            showNotification("Playing: " + titleFin);
+
+        }
+
+    }
+
+    /*
+    Backup
+        private class MetaDataTask extends TimerTask {
         private Scraper _scraper = new ShoutCastScraper();
         private Stream _stream;
         @Override
@@ -221,5 +299,6 @@ public class RadioService extends Service implements MediaPlayer.OnPreparedListe
         }
 
     }
+    */
 
 }
